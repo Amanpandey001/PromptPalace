ebfc4988a733da53b20f2e45f78e0a054467b320
Amanpandey001 <154779482+Amanpandey001@users.noreply.github.com>
2024-11-17 21:26:27 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,9 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    experimental: {
+    missingSuspenseWithCSRBailout: false,
+    },
   };
   
   export default nextConfig;
